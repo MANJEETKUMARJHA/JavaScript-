f4b9695222af4848a136dad1a410d3aa052a2ae0
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-02-07 12:17:20 +0530

    Data Type

---
+++ 01basic/02DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // tret all JS code as new version
+
+// alert(3 +3) //we are using node.js
+
+let name = "manjeet"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// String => " "
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => to find unique 
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
