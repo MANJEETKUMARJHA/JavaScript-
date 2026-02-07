48905cdcbebd2ce76124c2884743ea4c3477c9cd
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-02-07 11:58:27 +0530

    change

--- 01basic/01var.js
+++ 01basic/01var.js
@@ -1,2 +1,19 @@
-const accountID = 12345
-console.log(accountID)
+const accountID = 12345 //const is used to declear variable const can't change
+let accountEmail = "manjeetjaha@gmail.com" //let is used to declear variable
+var accountPassword = "12345" //we dont used to declear var
+accountCity = "Kolkata" //wrong way to declear variable
+let accountState;
+
+// accountID = 2 not allowed
+accountEmail = "hc@fdf.com"
+accountPassword = " 12456"
+accountCity = "jaipur"
+
+console.log(accountID);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
